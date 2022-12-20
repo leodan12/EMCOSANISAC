f30e0dce1fc589b25e9a131a65a79c713071ea87
leodan12 <leomacip4@gmail.com>
2022-12-20 03:32:40 -0500

    migraciones nuevas

--- database/seeders/InventarioSeeder.php
+++ database/seeders/InventarioSeeder.php
@@ -18,19 +18,19 @@ class InventarioSeeder extends Seeder
         DB::table('inventarios')->insert([
             'stock' => 10,
             'ubicacion' => 'repisa nro 5 level 10',
-            'producto_id' => 1,
+            'producto_id' => 2,
             'estado' => 1,
           ]);
           DB::table('inventarios')->insert([
             'stock' => 20,
             'ubicacion' => 'repisa nro 2 level 5',
-            'producto_id' => 1,
+            'producto_id' => 3,
             'estado' => 1,
           ]);
           DB::table('inventarios')->insert([
             'stock' => 30,
             'ubicacion' => 'repisa nro 3 level 7',
-            'producto_id' => 1,
+            'producto_id' => 4,
             'estado' => 1,
           ]);
           DB::table('inventarios')->insert([
