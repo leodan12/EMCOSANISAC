b6f52e2674ae8b60ff5418ff0abd5f362b091145
leodan12 <leomacip4@gmail.com>
2022-12-21 23:58:36 -0500

    Login cont

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\api\LoginController;
+use App\Http\Controllers\Api\LoginController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
