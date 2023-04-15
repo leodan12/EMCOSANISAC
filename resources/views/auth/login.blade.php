9c835e2a80376ba63dcd66f1558348decddd1f50
leodan12 <71913712+leodan12@users.noreply.github.com>
2023-04-15 01:25:19 -0500

    Update login.blade.php
    datos del user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,12 +16,12 @@
             @csrf
 
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-label for="email" value="{{ __('Email: usuario12@gmail.com') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="{{ __('Password: usuario12') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
